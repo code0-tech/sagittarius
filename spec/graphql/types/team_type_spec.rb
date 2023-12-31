642bd7271ba6c8e33002d360b0d6e30617156b9c
Niklas van Schrick <mc.taucher2003@gmail.com>
2023-12-31 18:39:38 +0100

    Add N+1 query spec for team members field

--- spec/graphql/types/team_type_spec.rb
+++ spec/graphql/types/team_type_spec.rb
@@ -16,4 +16,43 @@ RSpec.describe SagittariusSchema.types['Team'] do
   it { expect(described_class.graphql_name).to eq('Team') }
   it { expect(described_class).to have_graphql_fields(fields) }
   it { expect(described_class).to require_graphql_authorizations(:read_team) }
+
+  context 'when requesting members', type: :request do
+    include GraphqlHelpers
+
+    def resolve
+      post_graphql query, variables: variables, current_user: current_user
+    end
+
+    let(:query) do
+      <<~QUERY
+        query($teamId: TeamID) {
+          team(id: $teamId) {
+            members {
+              count
+              nodes {
+                id
+                user { username }
+                team { name }
+              }
+            }
+          }
+        }
+      QUERY
+    end
+
+    let(:current_user) { create(:user) }
+    let(:team) { create(:team).tap { |team| create(:team_member, team: team, user: current_user) } }
+    let(:variables) { { teamId: team.to_global_id.to_s } }
+
+    it 'prevents N+1 queries' do
+      resolve # warm-up
+
+      control = ActiveRecord::QueryRecorder.new { resolve }
+
+      create(:team_member, team: team)
+
+      expect { resolve }.not_to exceed_query_limit(control)
+    end
+  end
 end
---
+++ spec/support/helpers/query_recorder.rb
@@ -0,0 +1,123 @@
+# frozen_string_literal: true
+
+# Heavily inspired by the implementation of GitLab
+# (https://gitlab.com/gitlab-org/gitlab/-/blob/a08bdb5be306f4815ea543aa627ade44774f52df/spec/support/helpers/query_recorder.rb)
+# which is licensed under a modified version of the MIT license which can be found at
+# https://gitlab.com/gitlab-org/gitlab/-/blob/a08bdb5be306f4815ea543aa627ade44774f52df/LICENSE
+#
+# The code might have been modified to accommodate for the needs of this project
+
+module ActiveRecord
+  class QueryRecorder
+    attr_reader :log, :skip_cached, :skip_schema_queries, :cached, :data
+
+    UNKNOWN = %w[unknown unknown].freeze
+
+    def initialize(skip_cached: true, skip_schema_queries: true, log_file: nil, query_recorder_debug: false, &block)
+      @data = Hash.new { |h, k| h[k] = { count: 0, occurrences: [], backtrace: [], durations: [] } }
+      @log = []
+      @cached = []
+      @skip_cached = skip_cached
+      @skip_schema_queries = skip_schema_queries
+      @query_recorder_debug = ENV['QUERY_RECORDER_DEBUG'] || query_recorder_debug
+      @log_file = log_file.nil? ? nil : File.open(log_file, 'w')
+      record(&block) if block
+      @log_file&.close
+    end
+
+    def record(&block)
+      # force replacement of bind parameters to give tests the ability to check for ids
+      ActiveSupport::Notifications.subscribed(method(:callback), 'sql.active_record', &block)
+    end
+
+    def show_backtrace(values, duration)
+      values[:sql].lines.each do |line|
+        print_to_log(:SQL, line)
+      end
+      print_to_log(:DURATION, duration)
+      Rails.backtrace_cleaner.clean(caller_locations).each do |line|
+        print_to_log(:backtrace, line)
+      end
+    end
+
+    def print_to_log(label, line)
+      msg = "QueryRecorder #{label}:  --> #{line}"
+
+      if @log_file
+        @log_file.puts(msg)
+      else
+        Rails.logger.debug(msg) # rubocop:disable Sagittarius/Logs/RailsLogger -- this is only used in tests
+      end
+    end
+
+    def get_sql_source(sql)
+      matches = sql.match(%r{,line:(?<line>.*):in\s+`(?<method>.*)'\*/})
+      matches ? [matches[:line], matches[:method]] : UNKNOWN
+    end
+
+    def store_sql_by_source(values: {}, duration: nil, backtrace: nil)
+      full_name = get_sql_source(values[:sql]).join(':')
+      @data[full_name][:count] += 1
+      @data[full_name][:occurrences] << values[:sql]
+      @data[full_name][:backtrace] << backtrace
+      @data[full_name][:durations] << duration
+    end
+
+    def find_query(query_regexp, limit, first_only: false)
+      out = []
+
+      @data.each_pair do |k, v|
+        if v[:count] > limit && k.match(query_regexp)
+          out << [k, v[:count]]
+          break if first_only
+        end
+      end
+
+      out.flatten! if first_only
+      out
+    end
+
+    def occurrences_by_line_method
+      @occurrences_by_line_method ||= @data.sort_by { |_, v| v[:count] }
+    end
+
+    def callback(_name, start, finish, _message_id, values)
+      duration = finish - start
+
+      if values[:cached] && skip_cached
+        @cached << values[:sql]
+      elsif !ignorable?(values)
+
+        backtrace = @query_recorder_debug ? show_backtrace(values, duration) : nil
+        @log << values[:sql]
+        store_sql_by_source(values: values, duration: duration, backtrace: backtrace)
+      end
+    end
+
+    def count
+      @count ||= @log.count
+    end
+
+    def cached_count
+      @cached_count ||= @cached.count
+    end
+
+    def log_message
+      @log_message ||= @log.join("\n\n")
+    end
+
+    def occurrences
+      @occurrences ||= @log.group_by(&:to_s).transform_values(&:count)
+    end
+
+    def occurrences_starting_with(str)
+      occurrences.select { |query, _count| query.starts_with?(str) }
+    end
+
+    def ignorable?(values)
+      return true if skip_schema_queries && values[:name]&.include?('SCHEMA')
+
+      false
+    end
+  end
+end
