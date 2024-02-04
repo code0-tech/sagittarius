dcd62000d7cbefe403884456e4c772c3705d8f3e
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-02-04 21:18:58 +0100

    Fix pipeline to detect outdated db

--- spec/support/matchers/audit_event_matchers.rb
+++ spec/support/matchers/audit_event_matchers.rb
@@ -2,10 +2,9 @@
 
 RSpec::Matchers.define :create_audit_event do |type, payload = {}|
   payload.fetch(:details, {}).transform_keys!(&:to_s)
+  before_last_id = 0
 
   match do |event_proc|
-    before_last_id = 0
-
     if event_proc.is_a?(Proc)
       before_last_id = AuditEvent.where(action_type: type).last&.id&.+(1) || 0
       event_proc.call
@@ -25,6 +24,15 @@ RSpec::Matchers.define :create_audit_event do |type, payload = {}|
 
     message << "did not create audit event of type <#{type}>"
     message << "with payload: <#{payload.inspect}>"
+
+    new_events = AuditEvent.where(id: before_last_id..)
+    unless new_events.empty?
+      message << ''
+      message << 'Following audit events where created:'
+      new_events.each do |event|
+        message << event.inspect
+      end
+    end
 
     message.join("\n")
   end
