c0db4e5f90ccc1b8bf8918671d8dc4d0c7779673
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-03-30 23:07:36 +0100

    Allow orchestrator tasks in startup without db

--- config/initializers/application_settings.rb
+++ config/initializers/application_settings.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.to_prepare do
   next unless Rails.env.production?
-  next if ARGV.grep(/db:/).any?
+  next if ARGV.grep(/db:|orchestrator:/).any?
 
   ApplicationSetting.assert_settings_present!
 end
