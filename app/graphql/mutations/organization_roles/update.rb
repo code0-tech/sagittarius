26e3be58da48092b8d0ffd71e700445bb256bc1d
Dario <derioreal63@gmail.com>
2024-04-14 21:16:42 +0200

    update argument name, remove method parameter

--- app/graphql/mutations/organization_roles/update.rb
+++ app/graphql/mutations/organization_roles/update.rb
@@ -8,11 +8,11 @@ module Mutations
       field :organization_role, Types::OrganizationRoleType, null: true, description: 'The updated organization role.'
 
       argument :name, String, required: true, description: 'Name for the new organization.'
-      argument :organization_role, Types::GlobalIdType[::OrganizationRole],
+      argument :organization_role_id, Types::GlobalIdType[::OrganizationRole],
                required: true,
                description: 'ID of the organization role to update.'
 
-      def resolve(organisation: ,organization_role_id:, **params)
+      def resolve(organization_role_id:, **params)
         organization_role = SagittariusSchema.object_from_id(organization_role_id)
 
         return { organization_role: nil, errors: [create_message_error('Invalid organization role')] } if organization_role.nil?
