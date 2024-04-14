8c5dfdf4ed4cb4f49a2adfa91ea7e4cdff780670
Dario <derioreal63@gmail.com>
2024-04-14 11:49:20 +0200

    started to add mutation to update organization roles

---
+++ app/graphql/mutations/organization_roles/update.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Mutations
+  module OrganizationsRoles
+    class Update < BaseMutation
+      description 'Update an existing organisation role.'
+
+      field :organization_role, Types::OrganizationRoleType, null: true, description: 'The updated organization role.'
+
+      argument :name, String, required: true, description: 'Name for the new organization.'
+      argument :organization_role, Types::GlobalIdType[::OrganizationRole],
+               required: true,
+               description: 'ID of the organization role to update.'
+
+      def resolve(organisation: ,organization_role_id:, **params)
+        organization_role = SagittariusSchema.object_from_id(organization_role_id)
+
+        return { organization_role: nil, errors: [create_message_error('Invalid organization role')] } if organization_role.nil?
+
+        ::OrganizationRoles::UpdateService.new(
+          current_user,
+          organization_role,
+          params
+        ).execute.to_mutation_response(success_key: :organization_role)
+      end
+    end
+  end
+end
