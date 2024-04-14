23e0395842d01c42007b97c9d7009da355580dd6
Dario <derioreal63@gmail.com>
2024-04-14 20:36:00 +0200

    Fix Module name of the organization role update mutation

--- app/graphql/mutations/organization_roles/update.rb
+++ app/graphql/mutations/organization_roles/update.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Mutations
-  module OrganizationsRoles
+  module OrganizationRoles
     class Update < BaseMutation
       description 'Update an existing organisation role.'
 
