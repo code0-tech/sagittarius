2f0d1ba968ed7223275b02a03c38c15f62dd4832
Dario <derioreal63@gmail.com>
2024-04-20 01:49:51 +0200

    add some test cases for audit events and cleanup code

--- spec/services/users/login_service_spec.rb
+++ spec/services/users/login_service_spec.rb
@@ -3,33 +3,66 @@
 require 'rails_helper'
 
 RSpec.describe Users::LoginService do
-  subject(:service_response) { described_class.new(email: email, password: password).execute }
+  subject(:service_response) { described_class.new(**params).execute }
 
   context 'when the credentials are correct' do
     let(:email) { generate(:email) }
+    let(:username) { generate(:username) }
     let(:password) { generate(:password) }
-    let!(:current_user) { create(:user, email: email, password: password) }
+    let!(:current_user) { create(:user, email: email, username: username, password: password) }
 
-    it 'returns a success response' do
-      expect(service_response).to be_success
-      expect(service_response.payload).to be_a(UserSession)
-      expect(service_response.payload.user.id).to eq(current_user.id)
-      expect(service_response.payload.token).to be_present
+    def check_audit_events(user, key, other_key)
       is_expected.to create_audit_event(
         :user_logged_in,
-        author_id: current_user.id,
+        author_id: user.id,
         entity_type: 'User',
-        entity_id: current_user.id,
-        details: { email: current_user.email, method: 'username_and_password' },
+        entity_id: user.id,
+        details: { key => current_user.send(key), method: 'username_and_password' },
         target_type: 'User',
-        target_id: current_user.id
+        target_id: user.id
       )
+      is_expected.not_to create_audit_event(
+        :user_logged_in,
+        details: { other_key: current_user.send(other_key), method: 'username_and_password' }
+      )
+    end
+
+    shared_examples 'check correct credentials' do
+      it do
+        expect(service_response).to be_success
+        expect(service_response.payload).to be_a(UserSession)
+        expect(service_response.payload.user.id).to eq(current_user.id)
+        expect(service_response.payload.token).to be_present
+      end
+    end
+
+    context 'when logging in with email' do
+      let(:params) { { email: email, password: password } }
+
+      it_behaves_like 'check correct credentials'
+
+      # rubocop:disable RSpec/NoExpectationExample
+      it 'returns a success response' do
+        check_audit_events(current_user, :email, :username)
+      end
+    end
+
+    context 'when logging in with username' do
+      let(:params) { { username: username, password: password } }
+
+      it_behaves_like 'check correct credentials'
+
+      it 'returns a success response' do
+        check_audit_events(current_user, :username, :email)
+      end
+      # rubocop:enable RSpec/NoExpectationExample
     end
   end
 
   context 'when the credentials are incorrect' do
     let(:email) { generate(:email) }
     let(:password) { generate(:password) }
+    let(:params) { { email: email, password: password } }
 
     it 'returns an error response' do
       expect(service_response).to be_error
