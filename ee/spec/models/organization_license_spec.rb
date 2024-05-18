58bf8f3002bd390ad7374e3b69103eb51e6e492f
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-05-19 00:16:59 +0200

    Implement license loading

--- ee/spec/models/organization_license_spec.rb
+++ ee/spec/models/organization_license_spec.rb
@@ -30,4 +30,62 @@ RSpec.describe OrganizationLicense do
       it { is_expected.not_to be_valid }
     end
   end
+
+  describe '#license' do
+    it 'memoizes loaded license' do
+      allow(Code0::License).to receive(:load).and_return(SecureRandom.hex, SecureRandom.hex)
+
+      loaded = organization_license.license
+
+      expect(organization_license.license).to be loaded
+      expect(Code0::License).to have_received(:load)
+    end
+  end
+
+  describe '.current' do
+    let(:first_license) { create(:organization_license) }
+    let(:second_license) { create(:organization_license) }
+
+    after do
+      described_class.clear_memoize(:current)
+      described_class.clear_memoize(:current_reset_on_change)
+    end
+
+    it 'memoizes license' do
+      allow(described_class).to receive(:load_license)
+
+      described_class.current(first_license.organization)
+      described_class.current(first_license.organization) # make a memoized call
+
+      expect(described_class).to have_received(:load_license)
+    end
+
+    it 'does not memoize license from wrong organization' do
+      expect(described_class.current(first_license.organization)).to eq(first_license)
+      expect(described_class.current(second_license.organization)).to eq(second_license)
+    end
+  end
+
+  describe '.load_license' do
+    let(:organization) { create(:organization) }
+
+    it 'returns newest license' do
+      create(:organization_license, organization: organization)
+      current_license = create(:organization_license, organization: organization)
+
+      expect(described_class.load_license(organization)).to eq(current_license)
+    end
+
+    it 'ignores future licenses' do
+      current_license = create(:organization_license, organization: organization)
+      create(
+        :organization_license,
+        organization: organization,
+        start_date: Time.zone.today + 1,
+        end_date: Time.zone.today + 2
+      )
+
+      expect(described_class.load_license(organization)).to eq(current_license)
+    end
+  end
 end
