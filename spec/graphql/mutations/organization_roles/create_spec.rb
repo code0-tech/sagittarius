60632695cef6d685b1614fc81a5366e1aa86f9b5
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-06-13 21:49:56 +0200

    Migrate graphql mutations specs to namespaces

--- spec/graphql/mutations/organization_roles/create_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe Mutations::OrganizationRoles::Create do
-  it { expect(described_class.graphql_name).to eq('OrganizationRolesCreate') }
-end
--- spec/graphql/mutations/organization_roles/delete_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe Mutations::OrganizationRoles::Delete do
-  it { expect(described_class.graphql_name).to eq('OrganizationRolesDelete') }
-end
