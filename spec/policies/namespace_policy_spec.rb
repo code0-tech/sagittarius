9a8b713ed3f2a08f68af6a7582927f3f0050fd01
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-06-13 21:47:06 +0200

    Migrate policy specs to namespaces

---
+++ spec/policies/namespace_policy_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+RSpec.describe NamespacePolicy do
+  subject { described_class.new(current_user, namespace) }
+
+  context 'when user is member of the namespace' do
+    let(:current_user) { create(:user) }
+    let(:namespace) do
+      create(:namespace).tap do |namespace|
+        create(:namespace_member, namespace: namespace, user: current_user)
+      end
+    end
+
+    it { is_expected.to be_allowed(:read_namespace) }
+    it { is_expected.to be_allowed(:read_namespace_member) }
+    it { is_expected.to be_allowed(:read_namespace_member_role) }
+    it { is_expected.to be_allowed(:read_namespace_role) }
+  end
+
+  context 'when user is not member of the namespace' do
+    let(:current_user) { create(:user) }
+    let(:namespace) { create(:namespace) }
+
+    it { is_expected.not_to be_allowed(:read_namespace) }
+    it { is_expected.not_to be_allowed(:read_namespace_member) }
+    it { is_expected.not_to be_allowed(:read_namespace_member_role) }
+    it { is_expected.not_to be_allowed(:read_namespace_role) }
+  end
+
+  context 'when user is nil' do
+    let(:current_user) { nil }
+    let(:namespace) { create(:namespace) }
+
+    it { is_expected.not_to be_allowed(:read_namespace) }
+    it { is_expected.not_to be_allowed(:read_namespace_member) }
+    it { is_expected.not_to be_allowed(:read_namespace_member_role) }
+    it { is_expected.not_to be_allowed(:read_namespace_role) }
+  end
+end
--- spec/policies/organization_policy_spec.rb
+++ spec/policies/organization_policy_spec.rb
@@ -6,32 +6,17 @@ RSpec.describe OrganizationPolicy do
   subject { described_class.new(current_user, organization) }
 
   let(:current_user) { nil }
+  let(:organization) { create(:organization) }
 
-  context 'when user is member of the organization' do
+  context 'when user can read namespace' do
     let(:current_user) { create(:user) }
-    let(:organization) do
-      create(:organization).tap do |organization|
-        create(:organization_member, organization: organization, user: current_user)
-      end
+
+    before do
+      create(:namespace_member, user: current_user, namespace: organization.ensure_namespace)
     end
 
     it { is_expected.to be_allowed(:read_organization) }
-    it { is_expected.to be_allowed(:read_organization_member) }
   end
 
-  context 'when user is not member of the organization' do
-    let(:current_user) { create(:user) }
-    let(:organization) { create(:organization) }
-
-    it { is_expected.not_to be_allowed(:read_organization) }
-    it { is_expected.not_to be_allowed(:read_organization_member) }
-  end
-
-  context 'when user is nil' do
-    let(:current_user) { nil }
-    let(:organization) { create(:organization) }
-
-    it { is_expected.not_to be_allowed(:read_organization) }
-    it { is_expected.not_to be_allowed(:read_organization_member) }
-  end
+  it { is_expected.not_to be_allowed(:read_organization) }
 end
