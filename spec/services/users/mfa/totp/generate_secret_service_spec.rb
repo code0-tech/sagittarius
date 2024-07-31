7461aa9b9265b9cac17e85927b3f0b483567b0c4
Dario <derioreal63@gmail.com>
2024-07-31 20:00:21 +0200

    add missing tests

--- spec/services/users/mfa/totp/generate_secret_service_spec.rb
+++ spec/services/users/mfa/totp/generate_secret_service_spec.rb
@@ -13,13 +13,22 @@ RSpec.describe Users::Mfa::Totp::GenerateSecretService do
   end
 
   context 'when user is valid' do
-    let(:current_user) { create(:user) }
+    context 'totp secret is already set' do
+      let(:current_user) { create(:user, totp_secret: ROTP::Base32.random) }
 
-    it { is_expected.to be_success }
-
-    it 'is valid totp secret' do
-      totp = ROTP::TOTP.new(service_response.payload.split('--').first)
-      expect(totp.secret.length).to eq(48)
+      it { is_expected.to_not be_success }
+      it { expect(service_response.payload).to eq(:totp_secret_already_set) }
     end
+    context 'totp secret is not set' do
+      let(:current_user) { create(:user) }
+
+      it { is_expected.to be_success }
+
+      it 'is valid totp secret' do
+        totp = ROTP::TOTP.new(service_response.payload.split('--').first)
+        expect(totp.secret.length).to eq(48)
+      end
+    end
+
   end
 end
--- spec/services/users/mfa/totp/validate_secret_service_spec.rb
+++ spec/services/users/mfa/totp/validate_secret_service_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe Users::Mfa::Totp::ValidateSecretService do
     it { is_expected.not_to be_success }
     it { expect(service_response.payload).to eq(:missing_permission) }
   end
+  context 'when user is valid but totp secret is already set' do
+    let(:current_user) { create(:user, totp_secret: ROTP::Base32.random) }
+    let(:current_totp) { nil }
+    let(:signed_secret) { nil }
+
+    it { is_expected.to_not be_success }
+    it { expect(service_response.payload).to eq(:totp_secret_already_set) }
+  end
 
   context 'when user and secret is valid but totp is not' do
     let(:current_user) { create(:user) }
