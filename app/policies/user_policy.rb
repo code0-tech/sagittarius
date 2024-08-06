09033ab32214e5f72a21641873f70f2ccf26c3f8
Dario <derioreal63@gmail.com>
2024-08-06 18:21:12 +0200

    implement service

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,10 +2,17 @@
 
 class UserPolicy < BasePolicy
   condition(:user_is_self) { @subject.id == @user&.id }
+  condition(:user_is_admin) { @user.is_admin? }
+
 
   rule { ~anonymous }.enable :read_user
 
+  rule { user_is_admin }.policy do
+    enable :update_user
+  end
+
   rule { user_is_self }.policy do
     enable :manage_mfa
+    enable :update_user
   end
 end
