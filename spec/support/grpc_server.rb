fd86c9a6ef348ae3278e666fd4ab839fb321e77d
Niklas van Schrick <mc.taucher2003@gmail.com>
2024-08-20 18:30:58 +0200

    Fix database cleaner configuration

--- spec/support/grpc_server.rb
+++ spec/support/grpc_server.rb
@@ -8,6 +8,8 @@ RSpec.configure do |config|
   # hook for that because we want to start the grpc server before any test
   # starts to run.
   config.define_derived_metadata do |metadata|
+    metadata[:disable_transaction] = true if metadata[:need_grpc_server]
+
     if metadata[:need_grpc_server] && server_instance.nil?
       server_instance = Sagittarius::Grpc::Launcher.new
       server_instance.start
