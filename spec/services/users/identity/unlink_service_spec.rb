96e8fce4fa18f7418c6041e58ca1841ae4e15bc6
Dario <derioreal63@gmail.com>
2024-09-22 17:09:31 +0200

    move let into before hook

--- spec/services/users/identity/unlink_service_spec.rb
+++ spec/services/users/identity/unlink_service_spec.rb
@@ -12,7 +12,10 @@ RSpec.describe Users::Identity::UnlinkService do
   context 'when user is valid' do
     let(:provider_id) { :google }
     let(:current_user) { create(:user) }
-    let!(:identity) { create(:user_identity, user: current_user, identifier: 'identifier', provider_id: :google) }
+
+    before do
+      create(:user_identity, user: current_user, identifier: 'identifier', provider_id: :google)
+    end
 
     it do
       expect { service_response }.to change { current_user.reload.user_identities.length }.by(-1)
