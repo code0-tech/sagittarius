334609762d18bbbb8f93b9df8ae36cbd8d3018ac
Dario Pranjic <derioreal63@gmail.com>
2025-05-26 00:29:57 +0200

    Update datatype model and runtime function (definition) models

--- app/models/function_generic_mapper.rb
+++ app/models/function_generic_mapper.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class FunctionGenericMapper < ApplicationRecord
-  belongs_to :source, class_name: 'DataTypeIdentifier', inverse_of: :function_generic_mappers
+  belongs_to :data_type_identifier, optional: true, inverse_of: :function_generic_mappers
   belongs_to :runtime_function_definition, class_name: 'RuntimeFunctionDefinition', optional: true,
                                            inverse_of: :generic_mappers
-  belongs_to :runtime_parameter_definition, class_name: 'RuntimeParameterDefinition', optional: true,
-                                            inverse_of: :function_generic_mappers
 
   validates :target, presence: true
+  validate :exactly_one_of_generic_key_or_data_type_identifier_id
+
+  private
+
+  def exactly_one_of_generic_key_or_data_type_identifier_id
+    values = [generic_key.present?, data_type_identifier_id.present?]
+    return if values.count(true) == 1
+
+    errors.add(:base, 'Exactly one of generic_key or data_type_identifier_id must be present')
+  end
 end
