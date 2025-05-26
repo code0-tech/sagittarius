dbd36653c540495f62b0c2aa8c2217adbd097e6f
Dario Pranjic <derioreal63@gmail.com>
2025-05-26 13:37:18 +0200

    Adjust models and tests

--- app/models/function_generic_mapper.rb
+++ app/models/function_generic_mapper.rb
@@ -4,6 +4,8 @@ class FunctionGenericMapper < ApplicationRecord
   belongs_to :data_type_identifier, optional: true, inverse_of: :function_generic_mappers
   belongs_to :runtime_function_definition, class_name: 'RuntimeFunctionDefinition', optional: true,
                                            inverse_of: :generic_mappers
+  belongs_to :runtime_parameter_definition, class_name: 'RuntimeParameterDefinition', optional: true,
+                                            inverse_of: :function_generic_mappers
 
   validates :target, presence: true
   validate :exactly_one_of_generic_key_or_data_type_identifier_id
@@ -11,7 +13,7 @@ class FunctionGenericMapper < ApplicationRecord
   private
 
   def exactly_one_of_generic_key_or_data_type_identifier_id
-    values = [generic_key.present?, data_type_identifier_id.present?]
+    values = [generic_key.present?, data_type_identifier.present?]
     return if values.count(true) == 1
 
     errors.add(:base, 'Exactly one of generic_key or data_type_identifier_id must be present')
--- spec/factories/generic_types.rb
+++ spec/factories/generic_types.rb
@@ -2,6 +2,7 @@
 
 FactoryBot.define do
   factory :generic_type do
+    runtime
     generic_mappers { [] }
     data_type_identifier { nil }
   end
