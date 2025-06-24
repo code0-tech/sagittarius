4ee9a529caa741675f9f894eb827b74816146b34
Dario Pranjic <derioreal63@gmail.com>
2025-06-24 21:10:43 +0200

    Refactor debug logging in validation services for clarity

--- app/services/namespaces/projects/flows/validation/data_type/data_type_rule_validation_service.rb
+++ app/services/namespaces/projects/flows/validation/data_type/data_type_rule_validation_service.rb
@@ -19,7 +19,7 @@ module Namespaces
             end
 
             def execute
-              logger.debug("Validating data type rule #{rule.id}")
+              logger.debug(message: "Validating data type rule", rule_id: rule.id)
 
               transactional do |t|
                 if rule.invalid?
