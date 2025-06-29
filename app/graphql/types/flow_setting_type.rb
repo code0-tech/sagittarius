97a66cbea9949b93962d13002a992fdffa4ac53e
Dario Pranjic <derioreal63@gmail.com>
2025-06-29 15:18:59 +0200

    Remove unused database_id method

--- app/graphql/types/flow_setting_type.rb
+++ app/graphql/types/flow_setting_type.rb
@@ -10,10 +10,6 @@ module Types
 
     field :value, GraphQL::Types::JSON, null: false, method: :object, description: 'The value of the flow setting'
 
-    def database_id
-      object.id.to_global_id.to_s
-    end
-
     id_field FlowSetting
     timestamps
   end
