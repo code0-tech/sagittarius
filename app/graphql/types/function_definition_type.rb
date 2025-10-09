be64bc0c65254f514522d6842459d00621e8221a
Dario Pranjic <derioreal63@gmail.com>
2025-10-09 21:09:59 +0200

    Add some missing graphql fields

--- app/graphql/types/function_definition_type.rb
+++ app/graphql/types/function_definition_type.rb
@@ -15,9 +15,20 @@ module Types
     field :descriptions, Types::TranslationType.connection_type, null: true, description: 'Description of the function'
     field :names, Types::TranslationType.connection_type, null: true, description: 'Name of the function'
 
+    field :deprecation_messages, Types::TranslationType.connection_type,
+          null: true,
+          description: 'Deprecation message of the function'
     field :documentations, Types::TranslationType.connection_type,
           null: true,
           description: 'Documentation of the function'
+
+    field :runtime_function_definition, Types::RuntimeFunctionDefinitionType,
+          null: true, description: 'Runtime function definition'
+
+    field :generic_keys, [String], null: true, description: 'Generic keys of the function'
+
+    field :throws_error, Boolean,
+          null: false, description: 'Indicates if the function can throw an error'
 
     id_field FunctionDefinition
     timestamps
---
+++ docs/graphql/object/genericcombinationstrategy.md
@@ -0,0 +1,16 @@
+---
+title: GenericCombinationStrategy
+---
+
+Represents a combination strategy with AND/OR logic used by a generic mapper.
+
+## Fields without arguments
+
+| Name | Type | Description |
+|------|------|-------------|
+| `createdAt` | [`Time!`](../scalar/time.md) | Time when this GenericCombinationStrategy was created |
+| `genericMapper` | [`GenericMapper`](../object/genericmapper.md) | The associated generic mapper, if any. |
+| `id` | [`GenericCombinationStrategyID!`](../scalar/genericcombinationstrategyid.md) | Global ID of this GenericCombinationStrategy |
+| `type` | [`GenericCombinationStrategyType!`](../enum/genericcombinationstrategytype.md) | The combination type ('AND' or 'OR'). |
+| `updatedAt` | [`Time!`](../scalar/time.md) | Time when this GenericCombinationStrategy was last updated |
+
