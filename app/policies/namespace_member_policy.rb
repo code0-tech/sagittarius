bb70a9f4b71c28100f6318cf04396d2e18ebd53a
Niklas van Schrick <mc.taucher2003@gmail.com>
2025-11-09 15:47:20 +0100

    Allow user to leave a namespace

--- app/policies/namespace_member_policy.rb
+++ app/policies/namespace_member_policy.rb
@@ -2,4 +2,8 @@
 
 class NamespaceMemberPolicy < BasePolicy
   delegate { subject.namespace }
+
+  condition(:member_is_self) { subject.user.id == user&.id }
+
+  rule { member_is_self }.enable :delete_member
 end
