586839c4bacb88caf67276ff6acca8fd0798c50f
Dario Pranjic <derioreal63@gmail.com>
2025-11-13 21:58:12 +0100

    fix some suggestions from copilot

--- spec/services/users/identity/register_service_spec.rb
+++ spec/services/users/identity/register_service_spec.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+  # frozen_string_literal: true
 
 require 'rails_helper'
 
@@ -58,7 +58,7 @@ RSpec.describe Users::Identity::RegisterService do
 
   shared_examples 'invalid user' do |error_code|
     it { is_expected.not_to be_success }
-    it { expect(service_response[:error_code]).to eq(error_code) }
+    it { expect(service_response.payload[:error_code]).to eq(error_code) }
     it { expect { service_response }.not_to create_audit_event }
   end
 
@@ -183,7 +183,7 @@ RSpec.describe Users::Identity::RegisterService do
     end
 
     it { is_expected.not_to be_success }
-    it { expect(service_response[:error_code]).to eq(:invalid_user_identity) }
+    it { expect(service_response.payload[:error_code]).to eq(:invalid_user_identity) }
     it { expect { service_response }.not_to create_audit_event }
     it { expect(service_response.payload[:details].full_messages).to include('Identifier has already been taken') }
   end
