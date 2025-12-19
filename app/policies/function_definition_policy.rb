bc5e49cfb503f79d3860e93faa10fed173abdc23
Niklas van Schrick <mc.taucher2003@gmail.com>
2025-12-19 16:54:01 +0100

    Fix authorization for FunctionDefinition and ParameterDefinition

--- app/policies/function_definition_policy.rb
+++ app/policies/function_definition_policy.rb
@@ -2,4 +2,8 @@
 
 class FunctionDefinitionPolicy < BasePolicy
   delegate { subject.runtime_function_definition }
+
+  rule { can?(:read_runtime_function_definition) }.policy do
+    enable :read_function_definition
+  end
 end
--- app/policies/parameter_definition_policy.rb
+++ app/policies/parameter_definition_policy.rb
@@ -2,4 +2,8 @@
 
 class ParameterDefinitionPolicy < BasePolicy
   delegate { subject.runtime_parameter_definition }
+
+  rule { can?(:read_runtime_parameter_definition) }.policy do
+    enable :read_parameter_definition
+  end
 end
