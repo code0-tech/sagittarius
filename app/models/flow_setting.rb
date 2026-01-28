ecc910e40c38be605761f72184ea150650dd7406
Dario Pranjic <96529060+Knerio@users.noreply.github.com>
2026-01-28 15:23:08 +0100

    Rename 'object' to 'value' in to_grpc method
    Signed-off-by: Dario Pranjic <96529060+Knerio@users.noreply.github.com>

--- app/models/flow_setting.rb
+++ app/models/flow_setting.rb
@@ -7,7 +7,7 @@ class FlowSetting < ApplicationRecord
     Tucana::Shared::FlowSetting.new(
       database_id: id,
       flow_setting_id: flow_setting_id,
-      object: Tucana::Shared::Value.from_ruby(object)
+      value: Tucana::Shared::Value.from_ruby(object)
     )
   end
 end
