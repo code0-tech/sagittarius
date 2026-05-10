8f388dc8a4be1b5648576ed88d6ebf9615e681d0
Raphael Götz <52959657+raphael-goetz@users.noreply.github.com>
2026-05-10 08:00:11 +0200

    Update db/migrate/20260509183408_add_readme_to_user.rb
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>
    Signed-off-by: Raphael Götz <52959657+raphael-goetz@users.noreply.github.com>

--- db/migrate/20260509183408_add_readme_to_user.rb
+++ db/migrate/20260509183408_add_readme_to_user.rb
@@ -2,6 +2,6 @@
 
 class AddReadmeToUser < Code0::ZeroTrack::Database::Migration[1.0]
   def change
-    add_column :users, :readme, :text, null: true
+    add_column :users, :readme, :text, null: true, limit: 5000
   end
 end
