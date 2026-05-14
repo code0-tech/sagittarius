ecb5a6119c6a5292f8296449e3287fc01a2e4173
Raphael <raphael.goetz.7@gmail.com>
2026-05-14 09:49:08 +0200

    feat: added grpc module handler

---
+++ app/grpc/module_handler.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class ModuleHandler < Tucana::Sagittarius::ModuleService::Service
+  include GrpcHandler
+  include Code0::ZeroTrack::Loggable
+
+  def update(request, _call)
+    current_runtime = Runtime.find(Code0::ZeroTrack::Context.current[:runtime][:id])
+
+    response = Runtimes::Grpc::Modules::UpdateService.new(current_runtime, request.modules).execute
+
+    logger.debug("ModuleHandler#update response: #{response.inspect}")
+
+    Tucana::Sagittarius::ModuleUpdateResponse.new(success: response.success?)
+  end
+end
