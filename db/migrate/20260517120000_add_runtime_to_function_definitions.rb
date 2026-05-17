d5b0f815cdd717bd7bc0c73161f76459f7589912
Raphael Götz <52959657+raphael-goetz@users.noreply.github.com>
2026-05-17 15:13:45 +0200

    Update db/migrate/20260517120000_add_runtime_to_function_definitions.rb
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>
    Signed-off-by: Raphael Götz <52959657+raphael-goetz@users.noreply.github.com>

--- db/migrate/20260517120000_add_runtime_to_function_definitions.rb
+++ db/migrate/20260517120000_add_runtime_to_function_definitions.rb
@@ -2,16 +2,13 @@
 
 class AddRuntimeToFunctionDefinitions < Code0::ZeroTrack::Database::Migration[1.0]
   def change
-    unless column_exists?(:function_definitions, :runtime_id)
-      add_reference :function_definitions, :runtime, foreign_key: { on_delete: :cascade }, index: false
-    end
+    add_reference :function_definitions, :runtime, foreign_key: { on_delete: :cascade }, index: false
 
-    remove_index :function_definitions, name: 'idx_function_definitions_on_module_id_identifier',
-                                        if_exists: true
+    remove_index :function_definitions, name: 'idx_function_definitions_on_module_id_identifier'
+
     add_index :function_definitions,
               %i[runtime_id identifier],
               unique: true,
-              name: 'idx_function_definitions_on_runtime_id_identifier',
-              if_not_exists: true
+              name: 'idx_function_definitions_on_runtime_id_identifier'
   end
 end
