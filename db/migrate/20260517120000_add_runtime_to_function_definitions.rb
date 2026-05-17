b7b546bf152c2ca8717cc4814d5b8728f50360a8
Raphael <raphael.goetz.7@gmail.com>
2026-05-17 15:39:12 +0200

    fix: added missing columns for rollback

--- db/migrate/20260517120000_add_runtime_to_function_definitions.rb
+++ db/migrate/20260517120000_add_runtime_to_function_definitions.rb
@@ -4,7 +4,10 @@ class AddRuntimeToFunctionDefinitions < Code0::ZeroTrack::Database::Migration[1.
   def change
     add_reference :function_definitions, :runtime, foreign_key: { on_delete: :cascade }, index: false
 
-    remove_index :function_definitions, name: 'idx_function_definitions_on_module_id_identifier'
+    remove_index :function_definitions,
+                 column: %i[runtime_module_id identifier],
+                 unique: true,
+                 name: 'idx_function_definitions_on_module_id_identifier'
 
     add_index :function_definitions,
               %i[runtime_id identifier],
