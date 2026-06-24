0d6136378a3d6045644288862af3b5007b7e5489
Raphael <raphael.goetz.7@gmail.com>
2026-06-24 13:29:17 +0200

    feat: added migration for increased type limits

---
+++ db/migrate/20260624120100_increase_type_limits_for_data_types_and_module_configuration_definitions.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class IncreaseTypeLimitsForDataTypesAndModuleConfigurationDefinitions < Code0::ZeroTrack::Database::Migration[1.0]
+  def change
+    change_column :data_types, :type, :text, null: false, limit: 8192
+    change_column :module_configuration_definitions, :type, :text, null: false, limit: 8192
+  end
+end
