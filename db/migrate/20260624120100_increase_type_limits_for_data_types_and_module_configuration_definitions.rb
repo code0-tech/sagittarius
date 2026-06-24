1384ed7a4f14f317bea89f4d723c53c2f7f98f48
Raphael <raphael.goetz.7@gmail.com>
2026-06-24 15:03:20 +0200

    feat: removed old constrains

--- db/migrate/20260624120100_increase_type_limits_for_data_types_and_module_configuration_definitions.rb
+++ db/migrate/20260624120100_increase_type_limits_for_data_types_and_module_configuration_definitions.rb
@@ -2,7 +2,10 @@
 
 class IncreaseTypeLimitsForDataTypesAndModuleConfigurationDefinitions < Code0::ZeroTrack::Database::Migration[1.0]
   def change
-    change_column :data_types, :type, :text, null: false, limit: 8192
-    change_column :module_configuration_definitions, :type, :text, null: false, limit: 8192
+    remove_check_constraint :data_types, 'char_length(type) <= 2000', name: 'check_01ca31b7b9'
+    remove_check_constraint :module_configuration_definitions, 'char_length(type) <= 2000', name: 'check_7fe4a3bc1a'
+
+    add_check_constraint :data_types, 'char_length(type) <= 8192', name: 'check_01ca31b7b9'
+    add_check_constraint :module_configuration_definitions, 'char_length(type) <= 8192', name: 'check_7fe4a3bc1a'
   end
 end
