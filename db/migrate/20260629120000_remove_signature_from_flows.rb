2f03ef041e6802de796bd52af1ee3330c0efa342
Raphael <raphael.goetz.7@gmail.com>
2026-06-29 10:06:53 +0200

    fix: change => up/down switch

--- db/migrate/20260629120000_remove_signature_from_flows.rb
+++ db/migrate/20260629120000_remove_signature_from_flows.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class RemoveSignatureFromFlows < Code0::ZeroTrack::Database::Migration[1.0]
-  def change
-    remove_column :flows, :signature, :text
+  def up
+    remove_column :flows, :signature
+  end
+
+  def down
+    add_column :flows, :signature, :text, null: false, default: '', limit: 500
   end
 end
