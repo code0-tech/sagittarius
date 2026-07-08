98c994e83d04c37675478f00837a8cb52b03092d
Raphael <raphael.goetz.7@gmail.com>
2026-07-08 18:59:49 +0200

    feat: implemented proxy server of module status

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,12 +1,31 @@
-fn main() {
-    println!("Hello, world!");
-}
+use tonic::transport::Channel;
 
-#[cfg(test)]
-mod tests {
+use crate::client::execution_service_client::SagittariusRailsExecutionServiceClient;
+use crate::client::flow_service_client::SagittariusRailsFlowServiceClient;
+use crate::client::module_service_client::SagittariusRailsModuleServiceClient;
+use crate::client::runtime_status_service_client::SagittariusRailsRuntimeStatusServiceClient;
+use crate::client::token_service_client::SagittariusRailsTokenServiceClient;
+use crate::config::Config;
 
-  #[test]
-  fn test_add() {
-    assert_eq!(1+1, 2);
-  }
+mod client;
+mod config;
+mod server;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config = Config::default();
+
+    let url = config.backend.url.clone();
+    let channel = Channel::from_shared(url)?;
+
+    let _rails_execution_client =
+        SagittariusRailsExecutionServiceClient::connect(channel.clone()).await?;
+    let _rails_flow_client = SagittariusRailsFlowServiceClient::connect(channel.clone()).await?;
+    let _rails_module_client =
+        SagittariusRailsModuleServiceClient::connect(channel.clone()).await?;
+    let _rails_status_client =
+        SagittariusRailsRuntimeStatusServiceClient::connect(channel.clone()).await?;
+    let _rails_token_client = SagittariusRailsTokenServiceClient::connect(channel.clone()).await?;
+
+    Ok(())
 }
--- gateway/src/server/runtime_status_service_server.rs
+++ gateway/src/server/runtime_status_service_server.rs
@@ -1,19 +1,44 @@
 #![allow(dead_code)]
 
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use tucana::sagittarius_gateway::runtime_status_service_server::RuntimeStatusService;
 use tucana::sagittarius_gateway::{RuntimeStatusUpdateRequest, RuntimeStatusUpdateResponse};
 
-pub struct SagittariusRuntimeStatusService {}
+use crate::client::runtime_status_service_client::SagittariusRailsRuntimeStatusServiceClient;
+
+pub struct SagittariusRuntimeStatusService {
+    client: Arc<Mutex<SagittariusRailsRuntimeStatusServiceClient>>,
+}
 
 #[tonic::async_trait]
 impl RuntimeStatusService for SagittariusRuntimeStatusService {
     async fn update(
         &self,
-        _request: tonic::Request<RuntimeStatusUpdateRequest>,
+        request: tonic::Request<RuntimeStatusUpdateRequest>,
     ) -> Result<tonic::Response<RuntimeStatusUpdateResponse>, tonic::Status> {
+        let status_request = convert_status_update_request(request);
+
+        let response = {
+            let mut client_lock = self.client.lock().await;
+            client_lock.update(status_request).await
+        }?;
+
+        let status_response = response.into_inner();
+
         Ok(tonic::Response::new(RuntimeStatusUpdateResponse {
-            success: true,
-            error: None,
+            success: status_response.success,
+            error: status_response.error,
         }))
+    }
+}
+
+fn convert_status_update_request(
+    request: tonic::Request<RuntimeStatusUpdateRequest>,
+) -> tucana::sagittarius_rails::RuntimeStatusUpdateRequest {
+    let status_request = request.into_inner();
+
+    tucana::sagittarius_rails::RuntimeStatusUpdateRequest {
+        status: status_request.status,
     }
 }
