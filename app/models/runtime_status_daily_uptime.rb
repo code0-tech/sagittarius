0f16c7ab6aacc8aa5543a3fc4fabfd42c8d2dcfb
Niklas van Schrick <mc.taucher2003@gmail.com>
2026-08-13 22:32:30 +0200

    Switch daily uptime tables to natural composite primary key

--- app/models/runtime_status_daily_uptime.rb
+++ app/models/runtime_status_daily_uptime.rb
@@ -6,7 +6,7 @@ class RuntimeStatusDailyUptime < ApplicationRecord
   partition_by :date, strategy: :daily, retain_for: 15.days
 
   self.table_name = 'p_runtime_status_daily_uptimes'
-  self.primary_key = %i[id date]
+  self.primary_key = %i[runtime_status_id date]
 
   belongs_to :runtime_status, inverse_of: :daily_uptimes
 
