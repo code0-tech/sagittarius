3f0fb9fef67a4c095b7416dbfd7e6b088a99c893
Raphael <raphael.goetz.7@gmail.com>
2026-08-20 22:23:31 +0200

    feat: implemented retry inside of the grpc services

--- gateway/src/client/execution_service_client.rs
+++ gateway/src/client/execution_service_client.rs
@@ -4,13 +4,19 @@ use tonic::transport::{Channel, Endpoint};
 use tucana::sagittarius_rails::execution_service_client::ExecutionServiceClient;
 use tucana::sagittarius_rails::{ExecutionRequest, ExecutionResponse};
 
+use super::retry::{RetryPolicy, retry};
+
 #[derive(Clone)]
 pub struct SagittariusRailsExecutionServiceClient {
     inner: ExecutionServiceClient<Channel>,
+    retry_policy: RetryPolicy,
 }
 
 impl SagittariusRailsExecutionServiceClient {
-    pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
+    pub async fn connect<D>(
+        dst: D,
+        retry_policy: RetryPolicy,
+    ) -> Result<Self, tonic::transport::Error>
     where
         D: TryInto<Endpoint>,
         D::Error: Into<StdError>,
@@ -18,6 +24,7 @@ impl SagittariusRailsExecutionServiceClient {
         let channel = Endpoint::new(dst)?.connect_lazy();
         Ok(Self {
             inner: ExecutionServiceClient::new(channel),
+            retry_policy,
         })
     }
 
@@ -27,10 +34,13 @@ impl SagittariusRailsExecutionServiceClient {
         authorization: MetadataValue<tonic::metadata::Ascii>,
     ) -> Result<tonic::Response<ExecutionResponse>, tonic::Status> {
         log::debug!("Proxying a execution response.");
-        let mut request = tonic::Request::new(request);
-        request
-            .metadata_mut()
-            .insert("authorization", authorization);
-        self.inner.clone().update(request).await
+        retry(&self.retry_policy, || {
+            let mut inner = self.inner.clone();
+            let mut req = tonic::Request::new(request.clone());
+            req.metadata_mut()
+                .insert("authorization", authorization.clone());
+            async move { inner.update(req).await }
+        })
+        .await
     }
 }
--- gateway/src/client/module_service_client.rs
+++ gateway/src/client/module_service_client.rs
@@ -4,13 +4,19 @@ use tonic::transport::{Channel, Endpoint};
 use tucana::sagittarius_rails::module_service_client::ModuleServiceClient;
 use tucana::sagittarius_rails::{ModuleUpdateRequest, ModuleUpdateResponse};
 
+use super::retry::{RetryPolicy, retry};
+
 #[derive(Clone)]
 pub struct SagittariusRailsModuleServiceClient {
     inner: ModuleServiceClient<Channel>,
+    retry_policy: RetryPolicy,
 }
 
 impl SagittariusRailsModuleServiceClient {
-    pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
+    pub async fn connect<D>(
+        dst: D,
+        retry_policy: RetryPolicy,
+    ) -> Result<Self, tonic::transport::Error>
     where
         D: TryInto<Endpoint>,
         D::Error: Into<StdError>,
@@ -18,6 +24,7 @@ impl SagittariusRailsModuleServiceClient {
         let channel = Endpoint::new(dst)?.connect_lazy();
         Ok(Self {
             inner: ModuleServiceClient::new(channel),
+            retry_policy,
         })
     }
 
@@ -27,10 +34,13 @@ impl SagittariusRailsModuleServiceClient {
         authorization: MetadataValue<tonic::metadata::Ascii>,
     ) -> Result<tonic::Response<ModuleUpdateResponse>, tonic::Status> {
         log::debug!("Proxying a module update request.");
-        let mut request = tonic::Request::new(request);
-        request
-            .metadata_mut()
-            .insert("authorization", authorization);
-        self.inner.clone().update(request).await
+        retry(&self.retry_policy, || {
+            let mut inner = self.inner.clone();
+            let mut req = tonic::Request::new(request.clone());
+            req.metadata_mut()
+                .insert("authorization", authorization.clone());
+            async move { inner.update(req).await }
+        })
+        .await
     }
 }
