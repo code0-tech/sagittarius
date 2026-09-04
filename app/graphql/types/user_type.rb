537851f7a8614815f7be1184b428b9f84f4244d4
Raphael <raphael.goetz.7@gmail.com>
2026-09-04 20:30:14 +0200

    ref: corrected rubocop detection

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -47,12 +47,11 @@ module Types
           description: 'Identities of this user',
           method: :user_identities
 
-    # rubocop:disable GraphQL/ExtractType -- namespace pins are directly on the user, not a nested namespace type
+    # rubocop:disable-next GraphQL/ExtractType -- namespace pins are directly on the user, not a nested namespace type
     field :namespace_pins, [Types::UserNamespacePinType],
           null: false,
           description: 'Pinned namespaces of this user, ordered by priority',
           method: :user_namespace_pins
-    # rubocop:enable GraphQL/ExtractType
 
     field :mfa_status, Types::MfaStatusType,
           null: true,
