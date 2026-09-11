5682abf104e6e809d4b4e3a83f170316068d1b1f
Raphael Götz <52959657+raphael-goetz@users.noreply.github.com>
2026-09-11 09:53:20 +0200

    ref: renamed class to deferrable
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>
    Signed-off-by: Raphael Götz <52959657+raphael-goetz@users.noreply.github.com>

--- db/migrate/20260909120000_change_sub_flow_starting_node_fk_to_cascade.rb
+++ db/migrate/20260909120000_change_sub_flow_starting_node_fk_to_cascade.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ChangeSubFlowStartingNodeFkToCascade < Code0::ZeroTrack::Database::Migration[1.0]
+class ChangeSubFlowStartingNodeFkToDeferrable < Code0::ZeroTrack::Database::Migration[1.0]
   def change
     remove_foreign_key :sub_flows, :node_functions, column: :starting_node_id, on_delete: :restrict
 
