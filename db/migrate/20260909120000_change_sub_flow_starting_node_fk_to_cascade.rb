f2bd651944a6be38d86874ca9e7914e9afd11b68
Raphael Götz <52959657+raphael-goetz@users.noreply.github.com>
2026-09-11 09:38:08 +0200

    Apply batched suggestions from code review
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>
    Signed-off-by: Raphael Götz <52959657+raphael-goetz@users.noreply.github.com>

--- db/migrate/20260909120000_change_sub_flow_starting_node_fk_to_cascade.rb
+++ db/migrate/20260909120000_change_sub_flow_starting_node_fk_to_cascade.rb
@@ -2,7 +2,7 @@
 
 class ChangeSubFlowStartingNodeFkToCascade < Code0::ZeroTrack::Database::Migration[1.0]
   def change
-    remove_foreign_key :sub_flows, :node_functions, column: :starting_node_id
+    remove_foreign_key :sub_flows, :node_functions, column: :starting_node_id, on_delete: :restrict
 
     add_foreign_key :sub_flows, :node_functions, column: :starting_node_id, on_delete: :cascade
   end
