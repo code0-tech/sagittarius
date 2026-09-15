1664e25144d1f652e5c3ddacbc7de81ae9392e3a
Raphael <raphael.goetz.7@gmail.com>
2026-09-15 15:26:23 +0200

    feat: generalized execution results into flow based scope

--- app/graphql/subscriptions/namespaces/projects/flows/execution_result.rb
+++ app/graphql/subscriptions/namespaces/projects/flows/execution_result.rb
@@ -7,28 +7,18 @@ module Subscriptions
         class ExecutionResult < BaseSubscription
           description 'Subscription to asynchronously receive an execution result'
 
-          argument :execution_identifier,
-                   type: GraphQL::Types::String,
+          argument :flow_id,
+                   type: Types::GlobalIdType[Flow],
                    required: true,
-                   description: 'Execution identifier of the triggered execution'
+                   description: 'Id of the flow to receive execution results for'
 
           field :execution_result,
                 type: Types::ExecutionResultType,
                 null: true,
-                description: 'The execution result of the relevant execution'
-
-          def subscribe(execution_identifier:)
-            result = ::ExecutionResult.find_by(execution_identifier: execution_identifier, created_at: 1.hour.ago..)
-
-            if result.present?
-              unsubscribe({ execution_result: result })
-            else
-              :no_response
-            end
-          end
+                description: 'The most recent execution result of the relevant flow'
 
           def update(*)
-            unsubscribe({ execution_result: object })
+            { execution_result: object }
           end
         end
       end
